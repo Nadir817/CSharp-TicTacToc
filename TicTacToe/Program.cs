a29f3a59e6b13d5ad09926623af4a70e6b49e8f9
Nadir <nadir.erkin@gmail.com>
2023-04-14 10:49:12 -0400

    Basic logic finished. Needs to add win condition

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -2,11 +2,11 @@
 
 internal class Program
 {
-    static int[,] boards =
+    static char[,] boards =
     {
-        {1, 2, 3 },
-        { 4, 5, 6 },
-        { 7, 8, 9 },
+        {'1', '2', '3'},
+        {'4', '5', '6'},
+        {'7', '8', '9'},
     };
     public static void SetField()
     {
@@ -18,8 +18,50 @@ internal class Program
             Console.WriteLine("     |     |     ");
         }
     }
+
+    public static void PlacePiece(int player, int row, int col)
+    {
+        char piece = player == 1 ? 'x' : 'o';
+        boards[row, col] = piece;
+    }
+
+    public static bool CanPlace(int r, int c)
+    {
+        if (boards[r, c] != 'x' && boards[r,c] !='o')
+        {
+            return true;
+        } else { return false; }
+    }
+
     private static void Main(string[] args)
     {
-        SetField();
+        int player = 1;
+
+        
+        do
+        {
+            SetField();
+            Console.WriteLine("Player {0} please pick a field to place your piece: ", player);
+            int input;
+            bool success = Int32.TryParse(Console.ReadLine(), out input);
+            while (!success)
+            {
+                Console.WriteLine("Please pick a valid field");
+                success = Int32.TryParse(Console.ReadLine(), out input);
+            }
+            int row = (input - 1) / boards.GetLength(0);
+            int col = (input - 1) % boards.GetLength(0);
+            if (!CanPlace(row, col))
+            {
+                Console.Clear();
+                Console.WriteLine("Please pick non occupied field");
+                continue;
+            }
+            PlacePiece(player, row, col);
+            player = player == 1 ? 2 : 1;
+            Console.Clear();
+
+
+        } while (true);
     }
 }
