486ca22f83cdd4fd2f8dea367125d5fa5a0377b5
Nadir <nadir.erkin@gmail.com>
2023-04-14 10:00:03 -0400

    setting up sample board display

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+internal class Program
+{
+    static int[,] boards =
+    {
+        {1, 2, 3 },
+        { 4, 5, 6 },
+        { 7, 8, 9 },
+    };
+    public static void SetField()
+    {
+        for (int i = 0; i < boards.GetLength(0); i++)
+        {
+            Console.WriteLine("     |     |     ");
+            Console.WriteLine("  {0}  |  {1}  |  {2} ", boards[i, 0], boards[i, 1], boards[i, 2]);
+            Console.WriteLine("_____|_____|_____");
+            Console.WriteLine("     |     |     ");
+        }
+    }
+    private static void Main(string[] args)
+    {
+        SetField();
+    }
+}
